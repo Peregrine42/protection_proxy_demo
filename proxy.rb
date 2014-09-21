468d7d9d0b0c8b32d7754b1866852682bd6e7b1b
Duncan Fermor <duncan.fermor@gmail.com>
2014-09-21 21:50:28 +0100

    add authentication

--- proxy.rb
+++ proxy.rb
@@ -1,19 +1,30 @@
 class Proxy
 
-  def initialize subject
+  def initialize subject, auth
     @subject = subject
+    @auth    = auth
+  end
+
+  def authenticate!
+    raise ProtectionException.new('unauthorised') unless @auth.authenticate
   end
 
   def deposit amount
+    authenticate!
     @subject.deposit amount
   end
 
   def withdraw amount
+    authenticate!
     @subject.withdraw amount
   end
 
   def balance
+    authenticate!
     @subject.balance
   end
 
+end
+
+class ProtectionException < Exception
 end
--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -3,11 +3,26 @@ require_relative '../account'
 
 describe Proxy do
 
-  it 'delegates account operations to its subject' do
-    account = Proxy.new(Account.new)
-    account.deposit 50
-    account.withdraw 10
-    expect(account.balance).to eq 40
+  context 'user is ok' do
+
+    let!(:auth)    { double(:auth, authenticate: true) }
+    let!(:account) { Proxy.new(Account.new, auth)      }
+
+    it 'delegates account operations to its subject' do
+      account.deposit 50
+      account.withdraw 10
+      expect(account.balance).to eq 40
+    end
+
   end
 
+  context "it's a trap!" do
+
+    let!(:auth)    { double(:auth, authenticate: false) }
+    let!(:account) { Proxy.new(Account.new, auth)       }
+
+    it 'raises an error' do
+      expect { account.withdraw 10_000 }.to raise_error ProtectionException, 'unauthorised'
+    end
+  end
 end
